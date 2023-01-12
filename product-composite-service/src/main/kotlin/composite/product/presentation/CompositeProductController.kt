79fcc3ff1ae43354f5868b6af5317dbb935f9fd5
YoungChulShin <go1323@gmail.com>
2023-01-12 23:32:42 +0900

    composite: controller 추가 및 테스트를 위한 임시 코드 추가

--- product-composite-service/src/main/kotlin/composite/product/presentation/CompositeProductController.kt
+++ product-composite-service/src/main/kotlin/composite/product/presentation/CompositeProductController.kt
@@ -1,15 +1,18 @@
 package composite.product.presentation
 
 import common.model.composite.product.ProductAggregate
+import composite.product.service.CompositeProductService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CompositeProductController {
+class CompositeProductController(
+    private val compositeProductService: CompositeProductService,
+) {
 
-//    @GetMapping("/product-composite/{productId}")
-//    fun getProduct(@PathVariable productId: Int): ProductAggregate {
-//
-//    }
+    @GetMapping("/product-composite/{productId}")
+    fun getProduct(@PathVariable productId: Int): ProductAggregate {
+        return compositeProductService.getProduct(productId)
+    }
 }
--- product-service/src/main/kotlin/core/product/service/ProductService.kt
+++ product-service/src/main/kotlin/core/product/service/ProductService.kt
@@ -2,6 +2,8 @@ package core.product.service
 
 import common.model.core.product.Product
 import org.springframework.stereotype.Service
+import util.exceptions.InvalidInputException
+import util.exceptions.NotFoundException
 import util.http.ServiceUtil
 
 @Service
@@ -9,6 +11,10 @@ class ProductService(
     private val serviceUtil: ServiceUtil,
 ) {
 
-    fun getProduct(productId: Int) =
-        Product(productId, "name-${productId}", 123, serviceUtil.getServiceAddress())
+    fun getProduct(productId: Int): Product {
+        if (productId < 1) throw InvalidInputException("Invalid productId: $productId")
+        if (productId == 13) throw NotFoundException("No product found for productId: $productId")
+
+        return Product(productId, "name-${productId}", 123, serviceUtil.getServiceAddress())
+    }
 }
