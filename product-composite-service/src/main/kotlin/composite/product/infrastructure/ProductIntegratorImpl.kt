4001a110c897682bbf13697bf7d201acfc014455
YoungChulShin <go1323@gmail.com>
2023-01-13 07:24:04 +0900

    composite: Review, Recommendation 호출 시, 실패해도 기본 응답을 전달하도록 수정

--- product-composite-service/src/main/kotlin/composite/product/infrastructure/ProductIntegratorImpl.kt
+++ product-composite-service/src/main/kotlin/composite/product/infrastructure/ProductIntegratorImpl.kt
@@ -6,11 +6,17 @@ import common.model.core.recommendation.Recommendation
 import common.model.core.review.Review
 import composite.product.config.ServerConnectionConfig
 import composite.product.domain.ProductIntegrator
+import org.slf4j.LoggerFactory
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
+import util.exceptions.InvalidInputException
 import util.exceptions.NotFoundException
+import util.http.HttpErrorInfo
+import java.io.IOException
 
 @Component
 class ProductIntegratorImpl(
@@ -19,28 +25,70 @@ class ProductIntegratorImpl(
     private val serverConnectionConfig: ServerConnectionConfig,
 ) : ProductIntegrator {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(ProductIntegratorImpl::class.java)
+    }
+
     override fun getProduct(productId: Int): Product {
         val url = serverConnectionConfig.productService.getUrl("product/${productId}")
         return restTemplate.getForObject(url, Product::class.java) ?: throw NotFoundException()
     }
 
     override fun getRecommendations(productId: Int): List<Recommendation> {
-        val url = serverConnectionConfig.recommendationService.getUrl("recommendation?productId=${productId}")
-        return restTemplate.exchange(
-            url,
-            HttpMethod.GET,
-            null,
-            object : ParameterizedTypeReference<List<Recommendation>>(){}
-        ).body ?: listOf()
+        try {
+            val url = serverConnectionConfig.recommendationService.getUrl("recommendation?productId=${productId}")
+            return restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                object : ParameterizedTypeReference<List<Recommendation>>(){}
+            ).body ?: listOf()
+        } catch (ex: HttpClientErrorException) {
+            when(ex.statusCode) {
+                HttpStatus.NOT_FOUND -> throw NotFoundException(getErrorMessage(ex))
+                HttpStatus.UNPROCESSABLE_ENTITY -> throw InvalidInputException(getErrorMessage(ex))
+                else -> {
+                    logger.warn("Got a unexpected HTTP error: {}, will rethrow it", ex.statusCode)
+                    logger.warn("Error body: {}", ex.responseBodyAsString)
+                    throw ex
+                }
+            }
+        } catch (ex: Exception) {
+            logger.error("internal service error: {}", ex.message)
+            return listOf()
+        }
     }
 
     override fun getReviews(productId: Int): List<Review> {
-        val url = serverConnectionConfig.reviewService.getUrl("reviews?productId=${productId}")
-        return restTemplate.exchange(
-            url,
-            HttpMethod.GET,
-            null,
-            object : ParameterizedTypeReference<List<Review>>() { }
-        ).body ?: listOf()
+        try {
+            val url = serverConnectionConfig.reviewService.getUrl("reviews?productId=${productId}")
+            return restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                object : ParameterizedTypeReference<List<Review>>() { }
+            ).body ?: listOf()
+        } catch (ex: HttpClientErrorException) {
+            when(ex.statusCode) {
+                HttpStatus.NOT_FOUND -> throw NotFoundException(getErrorMessage(ex))
+                HttpStatus.UNPROCESSABLE_ENTITY -> throw InvalidInputException(getErrorMessage(ex))
+                else -> {
+                    logger.warn("Got a unexpected HTTP error: {}, will rethrow it", ex.statusCode)
+                    logger.warn("Error body: {}", ex.responseBodyAsString)
+                    throw ex
+                }
+            }
+        } catch (ex: Exception) {
+            logger.error("internal service error: {}", ex.message)
+            return listOf()
+        }
+    }
+
+    private fun getErrorMessage(ex: HttpClientErrorException): String {
+        return try {
+            objectMapper.readValue(ex.responseBodyAsString, HttpErrorInfo::class.java).message
+        } catch (ioEx: IOException) {
+            ex.message ?: ""
+        }
     }
 }
