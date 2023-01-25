bf9049b4d9430becb5e86864d40bb152eeb7ca79
YoungChulShin <go1323@gmail.com>
2023-01-26 00:08:35 +0900

    swagger 설정 추가

--- product-composite-service/src/main/kotlin/composite/product/config/SwaggerConfig.kt
+++ product-composite-service/src/main/kotlin/composite/product/config/SwaggerConfig.kt
@@ -2,17 +2,13 @@ package composite.product.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.Contact
-import springfox.documentation.service.StringVendorExtension
-import springfox.documentation.service.VendorExtension
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import kotlin.contracts.contract
 
 @Configuration
 @EnableSwagger2
@@ -25,17 +21,21 @@ class SwaggerConfig {
             .apis(RequestHandlerSelectors.basePackage("composite.product.presentation"))
             .paths(PathSelectors.any())
             .build()
-//            .apiInfo(ApiInfo(
-//                title = "my product",
-//                description = "my sample project",
-//                version = "1.0.0",
-//                termsOfServiceUrl = "",
-//                contact = Contact(
-//                    "shin yc",
-//                    "my url",
-//                    "my@gmail.com"),
-//                license = "license 2023",
-//                licenseUrl = "license url"
-//            ))
+            .apiInfo(
+                ApiInfo(
+                    "my product",
+                    "my sample project",
+                    "1.0.0",
+                    "",
+                    Contact(
+                        "shin yc",
+                        "my url",
+                        "my@gmail.com"
+                    ),
+                    "license 2023",
+                    "license url",
+                    emptyList()
+                )
+            )
     }
 }
--- product-composite-service/src/main/kotlin/composite/product/presentation/CompositeProductController.kt
+++ product-composite-service/src/main/kotlin/composite/product/presentation/CompositeProductController.kt
@@ -2,6 +2,9 @@ package composite.product.presentation
 
 import common.model.composite.product.ProductAggregate
 import composite.product.service.CompositeProductService
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +14,12 @@ class CompositeProductController(
     private val compositeProductService: CompositeProductService,
 ) {
 
+    @ApiOperation(value = "통합 제품 정보 조회")
+    @ApiResponses(
+        value = [
+            ApiResponse(code = 400, message = "Bad request, invalid format of request")
+        ]
+    )
     @GetMapping("/product-composite/{productId}")
     fun getProduct(@PathVariable productId: Int): ProductAggregate {
         return compositeProductService.getProduct(productId)
